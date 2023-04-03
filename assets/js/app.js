48f0cb036c8565f7672167f1cb8510f0700623b1
LookasWasTaken~ <piacen.luca@gmail.com>
2023-04-03 13:23:30 +0200

    ADD delete messages

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,3 @@
-/* Milestone 1
-Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
-Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
-Milestone 2
-Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
-Click sul contatto mostra la conversazione del contatto cliccato */
-
 const { createApp } = Vue;
 
 createApp({
@@ -275,6 +268,12 @@ createApp({
     },
     deleteMessage(index){
       this.contacts[this.activeUser].messages.splice((this.contacts[this.activeUser].messages.length -1) -index, 1)
+    },
+    deleteMessages(){
+      this.contacts[this.activeUser].messages = [];
+    },
+    deleteChat(){
+      this.contacts[this.activeUser] = [];
     },
     darkMode(){
       this.dark = !this.dark
